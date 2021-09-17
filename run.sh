7c36303e4d34aae69ec744b247439766bc410ee4
Derradji <89805807+derradjib76f@users.noreply.github.com>
2021-09-17 11:01:37 +0200

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,26 @@
+
+sleep 0.7
+clear
+
+echo -e "     \033[0;31m_______ _    _ ______ __  __ ______"
+echo -e "    \033[0;31m|__   __| |  | |  ____|  \/  |  ____|"
+echo -e "       \033[0;31m| |  | |__| | |__  | \  / | |__"
+echo -e "       \033[0;31m| |  |  __  |  __| | |\/| |  __|"
+echo -e "       \033[0;31m| |  | |  | | |____| |  | | |____"
+echo -e "       \033[0;31m|_|  |_|  |_|______|_|  |_|______|"
+
+echo -e "               \033[0;32mBY : Derradj.i"
+sleep 1
+echo ""
+   
+apt-get install cat -y
+cat T-Theme > bash.bashrc
+cp -f bash.bashrc ../../usr/etc
+                   
+echo ""             
+sleep 1
+echo -e "\033[0;33m            DONE !!"
+echo -e "\033[0;35m     THANK YOU AND GOOD LUCK"
+echo ""
+sleep 0.8
+
